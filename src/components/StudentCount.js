eda2eb7558293603b4ec29a3899e054b6b5d0c8b
Amarnathamraj <143314741+Amarnathamraj@users.noreply.github.com>
2024-04-17 12:59:39 +0530

    Create StudentCount.js
    added student count

---
+++ src/components/StudentCount.js
@@ -0,0 +1,10 @@
+import React from "react";
+const StudentCount = ({ count }) => {
+    return (
+      <div>
+        <p>Total Students Added: {count}</p>
+      </div>
+    );
+  };
+  
+  export default StudentCount;
